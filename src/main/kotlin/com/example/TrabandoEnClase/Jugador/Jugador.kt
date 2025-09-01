07bf201135d0fe57b3e0d1cc38137de85745c5fb
Gonzo <camilogonzalezherrea661@gmail.com>
2025-09-01 13:32:41 -0500

    se cambia

--- src/main/kotlin/com/example/TrabandoEnClase/Jugador/Jugador.kt
+++ src/main/kotlin/com/example/TrabandoEnClase/Jugador/Jugador.kt
@@ -1,2 +1,14 @@
-package com.example.TrabandoEnClase.Jugador 
+package com.example.TrabandoEnClase.Jugador
 
+data class Jugador(
+    val id: Int,
+    val nombre: String,
+    val n_documento: String,
+    val fecha_nacimiento: String,
+    val email: String,
+    val password: String,
+    val genero: String,
+    val edad: Int,
+    val user_id: Int,
+    val equipo_id: Int
+)
--- src/main/kotlin/com/example/TrabandoEnClase/torneo/TorneosController.kt
+++ src/main/kotlin/com/example/TrabandoEnClase/torneo/TorneosController.kt
@@ -1,2 +1,59 @@
-package com.example.TrabandoEnClase.torneo 
+package com.example.TrabandoEnClase.torneo
 
+import com.example.TrabandoEnClase.torneo.TorneosService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/torneos")
+class TorneosController {
+
+    @Autowired
+    lateinit var torneoService: TorneosService
+
+
+    @GetMapping
+    fun obtenerTorneos(): List<Torneo> {
+        return torneoService.obtenerTorneos()
+    }
+
+
+    @PostMapping
+    fun agregarTorneo(@RequestBody torneo: Torneo): String {
+        return try {
+            torneoService.agregarTorneo(torneo)
+            "Torneo agregado correctamente"
+        } catch (e: Exception) {
+            "Error al agregar el torneo: ${e.message}"
+        }
+    }
+
+
+    @PutMapping("/{id}")
+    fun actualizarTorneo(@PathVariable id: Int, @RequestBody torneo: Torneo): String {
+        return try {
+            torneoService.actualizarTorneo(torneo)
+            "Torneo actualizado correctamente"
+        } catch (e: Exception) {
+            "Error al actualizar el torneo: ${e.message}"
+        }
+    }
+
+
+    @DeleteMapping("/{id}")
+    fun eliminarTorneo(@PathVariable id: Int): String {
+        return try {
+            torneoService.eliminarTorneo(id)
+            "Torneo eliminado correctamente"
+        } catch (e: Exception) {
+            "Error al eliminar el torneo: ${e.message}"
+        }
+    }
+}
